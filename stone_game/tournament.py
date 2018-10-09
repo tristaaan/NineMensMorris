d0941518350d10cea82f7c406bac80dab2e5adb3
Andreas Olofsson <ao@blazar.se>
2018-10-09 22:21:09 +0200

    Simplified some code, added WIP player_next_match function.

--- stone_game/tournament.py
+++ stone_game/tournament.py
@@ -1,18 +1,16 @@
 import math
 import random
 
-import stone_game.terminalui as ui
-
 class Tournament:
     def __init__(self, players):
-        num_rounds = math.ceil(math.log2(len(players))) + 1
+        self.num_rounds = math.ceil(math.log2(len(players))) + 1
 
         random.shuffle(players)
 
         rounds = []
 
-        for round_i in range(num_rounds):
-            round_size = 2 ** (num_rounds - round_i - 1)
+        for round_i in range(self.num_rounds):
+            round_size = self.num_players_in_round(round_i)
 
             round = [None for _ in range(round_size)]
 
@@ -25,12 +23,30 @@ class Tournament:
                     while player_i < len(players) and slot_i < round_size:
                         round[slot_i] = players[player_i]
 
-                        player_i = player_i + 1
-                        slot_i = slot_i + 2
+                        player_i += 1
+                        slot_i += 2
 
-            rounds = rounds + [round]
+            rounds += [round]
 
-        self.num_rounds = num_rounds
-        self.round_winners = rounds
+        self.rounds_players = rounds
         self.current_round = 0
         self.current_match = 0
+
+    def num_players_in_round(self, round):
+        return 2 ** (self.num_rounds - round - 1)
+
+    def play_next_match(self):
+        if self.current_match >= self.num_rounds:
+            raise Exception("All rounds have already been played. ")
+
+        player1 = self.rounds_players[self.current_round][self.current_match * 2]
+        player2 = self.rounds_players[self.current_round][self.current_match * 2 + 1]
+
+        # TODO play match between player1 & player2
+        # TODO insert winning player into self.rounds_player[self.current_round + 1][self.current_match // 2]
+
+        self.current_round += 1
+
+        if self.current_match * 2 > self.num_players_in_round(self.current_round):
+            self.current_match = 0
+            self.current_round += 1
